bb551efad2f0465012022586f97c51864db5f7b1
James Demiraiakian <james.armen@gmail.com>
2022-02-24 17:56:52 -0800

    added REACT_APP_, and now reaches database

--- src/services/client.js
+++ src/services/client.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const client = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
+);
 
 export const parseData = ({ data, error }) => {
   if (error) throw error;
